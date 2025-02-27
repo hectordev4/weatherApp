e575059a7425cac591af7e173a0e4f7d1db0ca8b
Hectordev4 <hector.valverde.cana@gmail.com>
2025-02-27 13:13:27 +0100

    Features: restructure application with new routing and navigation components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import AppRoutes from './Routes';
-import ErrorBoundary from './components/ErrorBoundary';
+import { Navigate, Route, Routes, Router} from 'react-router-dom';
+import NavBar from './layouts/Navbar';
+import Home from './pages/Home';
+import Weather from './pages/Weather';
+import Map from './pages/Map';
+import About from './pages/About';
+import NoPage from './pages/NoPage';
+// import ErrorBoundary from './components/ErrorBoundary';
 
 export default function App() {
   return (
-    <ErrorBoundary>
-      <AppRoutes />
-    </ErrorBoundary>
+    <>
+    {/* <ErrorBoundary> */}
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/weather" element={<Weather />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    {/* </ErrorBoundary> */}
+    </>
   );
 }
 
