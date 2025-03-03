b3734acf27327e64a46bd5283e5fdaaed55c8232
Hectordev4 <hector.valverde.cana@gmail.com>
2025-03-03 08:33:38 +0100

    Features: update API key import to use environment variable for better security

--- src/api/api-rest.js
+++ src/api/api-rest.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { VITE_OPEN_WEATHER_API_KEY } from "/home/hector/Documents/myProjects/weatherApp/.env";
+const VITE_OPEN_WEATHER_API_KEY = process.env.VITE_OPEN_WEATHER_API_KEY;
 
 export const getWeather = async (city) => {
   const response = await axios.get(
